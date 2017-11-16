665012327f96efe00494e967bda3b57c40fa7177
klympotyuk <kim777@ukr.net>
2017-11-16 17:30:22 +0200

    selling canvas

--- assets/scripts/grid.js
+++ assets/scripts/grid.js
@@ -2,17 +2,6 @@ var grid = function(){
     var grid = document.getElementById('grid');
     var context = grid.getContext("2d");
     
-    for (var x = 0.5; x < 400; x += 10) {
-      context.moveTo(x, 0);
-      context.lineTo(x, 400);
-    }
     
-    for (var y = 0.5; y < 400; y += 10) {
-      context.moveTo(0, y);
-      context.lineTo(400, y);
-    }
-    
-    context.strokeStyle = "#888";
-    context.stroke();
 };
 grid();
