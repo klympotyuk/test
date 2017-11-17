45f03de76a2112a74b5b9d3ab18eec09dbb00395
klympotyuk <kim777@ukr.net>
2017-11-17 10:59:00 +0200

    selling canvas
    на маленьких розширеннях та великих працює не адекватно :(

--- assets/scripts/grid.js
+++ assets/scripts/grid.js
@@ -1,7 +1,28 @@
-var grid = function(){
-    var grid = document.getElementById('grid');
-    var context = grid.getContext("2d");
+$(window).on('load resize', function(){
+  var grid = function(){
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
     
+    var filld = document.getElementsByClassName('grid')[0];
+    var gridW =filld.offsetWidth;
+    var gridH =filld.offsetHeight;
+
+    canvas.style.width=gridW-37+"px";
+    canvas.style.height=gridH-37+"px";
+    var x=parseInt(canvas.style.width);
+    var y=parseInt(canvas.style.height);
     
-};
-grid();
+    for(var a=3;a<x;a+=80){
+      for(var b=3;b<x;b+=90){
+        context.beginPath();
+        context.arc(a, b, 2, 0, Math.PI*2, false); 
+        context.closePath();
+        context.fillStyle = "#889091"
+        context.fill();
+      }
+    }   
+  };
+  grid();
+});
+
+
